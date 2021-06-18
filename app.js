cb0a98a9259bd6e361eead8afeff0d22255ecfc5
bardenha <bardenha@gmail.com>
2021-06-18 23:09:34 +0100

    Added draw functionality

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const game = (() => {
 
     const reset = () => {
         gameboard = ['','','','','','','','',''];
+        gameController.round = 0;
         return render();
     }
+
+    //renders marker on the DOM
     const render = () => {
         for (i = 0; i < gameboard.length; i++) {
             const block = document.getElementById(`block${i}`);
@@ -22,33 +25,77 @@ const game = (() => {
             }
         }
     }
+
     const addMarker = (i) => {
         if (playerOneTurn && gameboard[i] == '') {
             gameboard[i] = 'x';
             playerOneTurn = false;
-            return render();
+            render();
+            gameController.checkRound();
         }
         else if (!playerOneTurn && gameboard[i] == '') {
             gameboard[i] = 'o'
             playerOneTurn = true;
-            return render();
+            render();
+            gameController.checkRound();
         }
         else {
             alert('Tile has already been selected!')
-            return render();
         }
+
+        //return render();
     }
 
     return {
         reset,
-        addMarker
+        addMarker,
+        gameboard
     };
 })();
 
 const gameController = (() => {
-    let round = 1;
+    let round = 0;
 
     const checkRound = () => {
+        if (gameController.round == 8 && !checkWin()) {
+            return setTimeout(() => {draw();}, 100);
+        }
+        else if (checkWin()) {
+            return win();
+        }
+        else {
+            gameController.round += 1;
+        }
+
+    }
+
+    const checkWin = () => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
         
     }
+
+    const draw = () => {
+        alert(`It's a draw!`);
+        return game.reset();
+    }
+
+    const win = () => {
+        alert(`Player x has won!`);
+        return game.reset();
+    }
+
+    return {
+        round,
+        checkRound
+    };
 })();
