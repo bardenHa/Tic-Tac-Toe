c22c4cd7c2b848d4c4c9f7365135b5fda9bbb01a
bardenha <bardenha@gmail.com>
2021-06-18 22:19:35 +0100

    Added ability to place markers with clicks

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
 const game = (() => {
-    let gameboard = ['x','o','x','x','o','x','x','o','x'];
+    let gameboard = ['','','','','','','','',''];
+    let playerOneTurn = true;
 
     const reset = () => {
         gameboard = ['','','','','','','','',''];
+        return render();
     }
     const render = () => {
         for (i = 0; i < gameboard.length; i++) {
             const block = document.getElementById(`block${i}`);
             const marker = gameboard[i];
-            console.log(block);
 
             if (marker == 'x') {
                 block.classList.add('fa-times', 'fa-10x');
-                console.log('x');
             }
             else if (marker == 'o') {
                 block.classList.add('fa-circle', 'fa-8x');
-                console.log('o');
             }
             else {
                 block.classList.remove('fa-times', 'fa-circle', 'fa-8x', 'fa-10x');
-                console.log('none');
             }
+        }
+    }
+    const addMarker = (i) => {
+        if (playerOneTurn && gameboard[i] == '') {
+            gameboard[i] = 'x';
+            playerOneTurn = false;
+            return render();
+        }
+        else if (!playerOneTurn && gameboard[i] == '') {
+            gameboard[i] = 'o'
+            playerOneTurn = true;
+            return render();
+        }
+        else {
+            alert('Tile has already been selected!')
+            return render();
         }
     }
 
     return {
         reset,
-        render
+        addMarker
     };
+})();
+
+const gameController = (() => {
+    let round = 1;
+
+    const checkRound = () => {
+        
+    }
 })();
